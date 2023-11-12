6ed0d25132a5254942764335109c6cfeede423a5
xpx <leilux.dev@gmail.com>
2023-11-13 00:42:05 +0800

    feature: init 2 demo.

---
+++ ipywidgets_vue/app-llama-dev.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "app-llama-dev"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ ipywidgets_vue/app-openai-playground.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "app-openai-playground"
+}
+</script>
+
+<style scoped>
+
+</style>
