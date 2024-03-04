a4db8f2d9dbc42f78f456f20e3e3b93f217490c6
xpx <leilux.dev@gmail.com>
2024-03-05 00:22:59 +0800

    add Tab component

---
+++ src/ipywui/components/TabPane.vue
@@ -0,0 +1,17 @@
+<template>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  name: "TabPane",
+  props: {
+    title: String,
+    name: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/ipywui/components/Tabs.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot></slot>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  props: {
+    tooltip: String,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
