92010e7f0a9075234bb948e9f23533ced406a9ca
xpx <leilux.dev@gmail.com>
2024-04-14 23:45:08 +0800

    doc: fix markdown basic example.

--- examples/ipywui/markdown_viewer/basic.vue
+++ examples/ipywui/markdown_viewer/basic.vue
@@ -1,10 +1,13 @@
 <template>
-  <Input v-model="md.value" type="textarea" style="height: 150px"></Input>
-  <MarkdownViewer :value="md.value"></MarkdownViewer>
+  <HBox>
+    <Input v-model="md.value" type="textarea" style="height: 150px"></Input>
+    <MarkdownViewer :value="md.value"></MarkdownViewer>
+  </HBox>
 </template>
 
 <script src="./basic_setup.py"></script>
 <script setup>
 import Input from "../../../src/ipywui/components/Input";
 import MarkdownViewer from "../../../src/ipywui/components/MarkdownViewer";
+import HBox from "../../../src/ipywui/components/HBox";
 </script>
