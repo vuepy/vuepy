e2c5852b418ec2db214b8684c012ca84818e1995
xpx <leilux.dev@gmail.com>
2024-06-25 23:00:43 +0800

    update: display doc

--- examples/ipywui/display/matplotlib_setup.py
+++ examples/ipywui/display/matplotlib_setup.py
@@ -4,50 +4,37 @@ import numpy as np
 from vuepy import ref
 
 
-def plt_sin():
-    x = np.arange(0, 5 * np.pi, 0.1)
-    y = np.sin(x)
-
-    plt.plot(x, y, color='green')
-    plt.xlabel('x')
-    plt.ylabel('sin(x)')
-    plt.title('Sine Curve using Matplotlib')
+def plt_to_img(title, xlabel, ylabel):
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
     plt.grid(True)
     # plt.show()
     im = plt.gcf()
     plt.close()
     return im
+
+
+def plt_sin():
+    x = np.arange(0, 5 * np.pi, 0.1)
+    y = np.sin(x)
+    plt.plot(x, y, color='green')
+    return plt_to_img('Sine Curve using Matplotlib', 'x', 'sin(x)')
 
 
 def plt_cos():
     x = np.arange(0, 5 * np.pi, 0.1)
     y = np.cos(x)
-
     plt.plot(x, y, color='blue')
-    plt.xlabel('x')
-    plt.ylabel('cos(x)')
-    plt.title('Cosine Curve using Matplotlib')
-    plt.grid(True)
-    # plt.show()
-    im = plt.gcf()
-    plt.close()
-    return im
+    return plt_to_img('Cosine Curve using Matplotlib', 'x', 'sin(x)')
 
 
 def plt_scatter():
     np.random.seed(1)
     data_x = np.random.randn(100)
     data_y = np.random.randn(100)
-
     plt.scatter(data_x, data_y, color='blue', alpha=0.5)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Scatter Plot Example')
-    plt.grid(True)
-    # plt.show()
-    im = plt.gcf()
-    plt.close()
-    return im
+    return plt_to_img('Scatter Plot Example', 'x', 'y')
 
 
 def setup(props, ctx, vm):
