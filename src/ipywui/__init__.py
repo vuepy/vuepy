8b0c7c605aa340b84fab203b4f27c67268b2d9b8
xpx <leilux.dev@gmail.com>
2024-10-16 00:42:47 +0800

    dugfix: add version for ipywui

--- src/ipywui/__init__.py
+++ src/ipywui/__init__.py
@@ -1,3 +1,6 @@
-from ipywui.core import wui
-from ipywui.core import IPywidgetsComponent
 from ipywui.comps import *
+from ipywui.core import IPywidgetsComponent
+from ipywui.core import wui
+from vuepy.version import VERSION
+
+__version__ = VERSION
