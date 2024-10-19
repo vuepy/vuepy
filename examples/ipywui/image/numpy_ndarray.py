9933d81e09adf6bc0ea9a7ec3529fd17e197a09a
xpx <leilux.dev@gmail.com>
2024-10-19 23:39:38 +0800

    feat: add convert_opencv_image_to_bin

--- examples/ipywui/image/numpy_ndarray.py
+++ examples/ipywui/image/numpy_ndarray.py
@@ -1,13 +1,14 @@
 import numpy as np
 from PIL import Image
 
+from vuepy.utils.image_processer import convert_opencv_image_to_bin
 from vuepy.utils.image_processer import convert_pil_image_to_bin
 
 
 def setup(props, ctx, vm):
     img_array = np.zeros([100, 100, 3], dtype=np.uint8)
     img_array[:, :, 2] = 200
-    # to rgb img
+    # ndarray to rgb img
     rgb_png = Image.fromarray(img_array, mode='RGB')
     # 必须指定format
     rgb_png.format = 'PNG'
@@ -15,15 +16,19 @@ def setup(props, ctx, vm):
 
     img_array = np.zeros([100, 100, 1], dtype=np.uint8)
     img_array += 122
-    # to gray img
+    # ndarray to gray img
     img = Image.fromarray(img_array.squeeze(), 'L')
     gray_png = convert_pil_image_to_bin(img, 'PNG')
 
 
     import cv2 as cv
     cv_img = cv.imread("jupyter_logo.png", cv.IMREAD_UNCHANGED)
-    # opencv to pil
+
+    # method1: opencv to pil
     img_png = Image.fromarray(cv.cvtColor(cv_img, cv.COLOR_BGRA2RGBA))
     img_png.format = "PNG"
+
+    # # method2: opencv to bin
+    # img_png = convert_opencv_image_to_bin(cv_img, '.png')
 
     return locals()
--- src/vuepy/utils/image_processer.py
+++ src/vuepy/utils/image_processer.py
@@ -1,7 +1,7 @@
 import io
 
 
-def convert_pil_image_to_bin(img, format=None):
+def convert_pil_image_to_bin(img, format=None) -> bytes:
     """
 
     :param img: PIL.Image
@@ -10,4 +10,17 @@ def convert_pil_image_to_bin(img, format=None):
     """
     with io.BytesIO() as buf:
         img.save(buf, format=format or img.format)
+        return buf.getvalue()
+
+
+def convert_opencv_image_to_bin(img, format='.png') -> bytes:
+    """
+
+    :param img: opencv img(ndarray)
+    :param format: .png .jpg
+    :return:
+    """
+    import cv2 as cv
+    _, img_encoded = cv.imencode(format, img)
+    with io.BytesIO(img_encoded.tobytes()) as buf:
         return buf.getvalue()
