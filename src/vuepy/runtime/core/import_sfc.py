2b5542c815660f9ced6f637d5ddad8a356bae21b
xpx <leilux.dev@gmail.com>
2025-02-12 01:41:50 +0800

    feat: 1. SFCFile add loads, init from raw_content.       2. import_sfc add raw_content support.       3. vuepy_import add cell magic       4. add vuepy_run line/cell magic to run Vuepy app

--- src/vuepy/runtime/core/import_sfc.py
+++ src/vuepy/runtime/core/import_sfc.py
@@ -7,8 +7,15 @@ from vuepy.compiler_sfc.codegen import SFCFactory
 from vuepy.compiler_sfc.compile import SFCFile
 
 
-def import_sfc(sfc_file):
-    sfc_file = SFCFile.load(sfc_file)
+def import_sfc(sfc_file, raw_content=False):
+    """
+    import Component from sfc_file or raw_content fo sfc
+
+    :param sfc_file:
+    :param raw_content:
+    :return:
+    """
+    sfc_file = SFCFile.loads(sfc_file) if raw_content else SFCFile.load(sfc_file)
 
     return SFCFactory(**{
         'setup': sfc_file.setup_fn,
